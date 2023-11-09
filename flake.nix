58dec42a122e7ab813560ab5ce8e94acb4f43e4b
Federico Ravasio <ravasio.federico@gmail.com>
2023-11-09 08:16:54 +0000

    upgrade(build): bump deps

--- flake.nix
+++ flake.nix
@@ -2,56 +2,56 @@
   description = "";
 
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
-    flake-utils = { url = "github:numtide/flake-utils"; };
+    nixpkgs = {url = "github:NixOS/nixpkgs/nixpkgs-unstable";};
+    flake-utils = {url = "github:numtide/flake-utils";};
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    {
-      overlays.default = nixpkgs.lib.composeManyExtensions [
-        poetry2nix.overlay
-        (final: prev: {
-          devEnv = prev.poetry2nix.mkPoetryEnv {
-            python = prev.python310;
-            projectDir = ./.;
-            editablePackageSources = {
-              "fs_schema_validator" = ./fs_schema_validator;
-            };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    poetry2nix,
+    ...
+  }: (flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs {inherit system;};
 
-            overrides = prev.poetry2nix.defaultPoetryOverrides.extend (self: super: {
-              types-pillow = super.types-pillow.overridePythonAttrs (
-                old: {
-                  buildInputs = (old.buildInputs or [ ]) ++ [ self.setuptools ];
-                }
-              );
-              svgelements = super.svgelements.overridePythonAttrs (
-                old: {
-                  buildInputs = (old.buildInputs or [ ]) ++ [ self.setuptools ];
-                }
-              );
-            });
-          };
-        })
+    poetry2nix_ = poetry2nix.lib.mkPoetry2Nix {inherit pkgs;};
+
+    devEnv = poetry2nix_.mkPoetryEnv {
+      projectDir = ./.;
+
+      editablePackageSources = {
+        "fs_schema_validator" = ./fs_schema_validator;
+      };
+
+      preferWheels = true;
+
+      overrides = poetry2nix_.defaultPoetryOverrides.extend (self: super: {
+        types-pillow = super.types-pillow.overridePythonAttrs (
+          old: {
+            buildInputs = (old.buildInputs or []) ++ [self.setuptools];
+          }
+        );
+
+        svgelements = super.svgelements.overridePythonAttrs (
+          old: {
+            buildInputs = (old.buildInputs or []) ++ [self.setuptools];
+          }
+        );
+      });
+    };
+  in {
+    devShells.default = pkgs.mkShell {
+      buildInputs = [
+        devEnv
+        pkgs.poetry
       ];
-    } // (flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.default ];
-        };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.devEnv
-            pkgs.poetry
-          ];
-        };
+    };
 
-        formatter = pkgs.nixpkgs-fmt;
-      }));
+    formatter = pkgs.alejandra;
+  }));
 }
